215f42cfa7f6361093e94ee96702ee203a1a795b
Sagar <sagarjadhav063@gmail.com>
2026-09-26 17:06:40 +0530

    chore: clean up temporary python scripts

--- patch_web_tools.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open("internal/agent/goal_executor.go", "r") as f:
-    content = f.read()
-
-content = content.replace("tr.Register(&tools.GitTool())", "tr.Register(&tools.GitTool())\n\ttr.Register(&tools.WebFetchTool())\n\ttr.Register(&tools.WebSearchTool())")
-
-with open("internal/agent/goal_executor.go", "w") as f:
-    f.write(content)
